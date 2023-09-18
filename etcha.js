8dddd25968eed107ae4e19309c9b5493b37bd44a
brekkeberg <brekke.berg@gmail.com>
2023-09-18 11:33:08 -0700

    redo main js script to isolate most actions into separate functions

--- etcha.js
+++ etcha.js
@@ -1,95 +1,111 @@
-//grabs main container so that grid can be populated
-const mainContainer = document.querySelector('#main');
+const DEFAULTGRIDSIZE = 16;
+const DEFAULTCOLOR = 'black';
 
 
-// creates array of all color buttons and all grid size buttons
+let gridSize = DEFAULTGRIDSIZE;
+let color = DEFAULTCOLOR;
+let userGridSizeInput = "";
+let userColorInput = "";
+
+//creates object that will contain grid
+const gridContainer = document.querySelector('#main');
+
+//builds initial default grid before any size button pushed
+buildGrid.call()
+
+// creates object containing all grid buttons and color buttons
 const allColorButtons = document.querySelectorAll('.colorButton');
 const allGridButtons = document.querySelectorAll('.gridButton');
-
-// creates event listener for all color choice buttons
-for (let i = 0; i <= allColorButtons.length - 1; i++){
-    allColorButtons[i].addEventListener('click', setColorChoice);
-}
-// logic for color choice buttons
-let userColorChoiceHex = "black";
-function setColorChoice(){
-    let userColorChoiceName = this.id;
-    if (userColorChoiceName === "colorRed"){
-        userColorChoiceHex = '#C44D58';
-    } else if (userColorChoiceName === "colorBlue"){
-        userColorChoiceHex = '#4ECDC4';
-    } else if (userColorChoiceName === "colorGreen"){
-        userColorChoiceHex = '#C7F464';
-    } else if (userColorChoiceName === "colorBlack"){
-        userColorChoiceHex = 'black';
-    } else if (userColorChoiceName === "colorRandom"){
-        userColorChoiceHex = 'RGB('+getRandColor()+',100,100)';
-    }
-}
-
 // creates event listener for all grid choice buttons
 for (let i = 0; i <= allGridButtons.length - 1; i++){
-    allGridButtons[i].addEventListener('click', makeEtchaGrid);
+    allGridButtons[i].addEventListener('click', resetGrid);
+}
+// creates event listener for all color choice buttons
+for (let i = 0; i <= allColorButtons.length - 1; i++){
+    allColorButtons[i].addEventListener('click', resetColor);
 }
 
+//function called when grid size button pushed
+function resetGrid(){
+    userGridSizeInput = this.id; //creates object to hold grid size button
+    setGridSize.call() //sets grid size based on button pushed
+    deleteGrid.call() //deletes old grid
+    buildGrid.call() //builds new grid
+}
 
-function makeEtchaGrid(){
-    //gets name of user grid choice size name from button click
-    let userGridChoiceName = this.id;
-    //converts grid choice size name to grid choice size number
-    function setGridSize(){
-        if(userGridChoiceName === "gridSmall"){
-            return 15;
-        } else if (userGridChoiceName === "gridMedium"){
-            return 30;
-        } else if (userGridChoiceName === "gridLarge"){
-            return 70;
-        }
-    }
-    let userGridChoiceNum = setGridSize()
-
-    //delets any previous grids that have been built
-    const allGridCols = document.querySelectorAll('.etchaCol');
-    for (let i = allGridCols.length - 1; i >= 0; i--){
-        for (let y = 0; y <= allGridCols.length -1; y++){
-            allGridCols[y].removeChild(allGridCols[y].lastChild);
-        mainContainer.removeChild(mainContainer.lastChild);
-        }
-    }
-
-    // constructs columns based on user grid choice size number
-    for (let i = 1; i <= userGridChoiceNum; i++){
-        let etchaCol = document.createElement('div');
-        etchaCol.classList.add('etchaCol');
-        mainContainer.appendChild(etchaCol);
+//builds new grid
+function buildGrid(){
+    for (let i = 0; i <= gridSize -1; i++){
+        let gridCol = document.createElement('div');
+        gridCol.classList.add('gridCol');
+        gridContainer.appendChild(gridCol);
         //constructs boxes within each column
-        for (let y = 1; y <= userGridChoiceNum; y++){
-            let etchaBox = document.createElement('div');
-            etchaBox.classList.add('etchaBox');
-            etchaCol.appendChild(etchaBox);
-            //adds event listener to each constructed box
-            etchaBox.addEventListener('mouseover', applyColor);
+        for (let y = 0; y <= gridSize -1; y++){
+            let gridBox = document.createElement('div');
+            gridBox.classList.add('gridBox');
+            gridCol.appendChild(gridBox);
+            //adds event listener to fill box with color if mouseover occurs
+            gridBox.addEventListener('mouseover', applyColor);
             function applyColor(){
-                etchaBox.style.cssText = "background-color: "+ userColorChoiceHex;
+                if (color === 'RANDOM'){
+                    gridBox.style.cssText = "background-color: " + getRandomColor();
+                }else{
+                    gridBox.style.cssText = "background-color: " + color;
+                } 
             }
         }
     }
 }
 
-function getRandColor(){
-    let randomRGBvalue = Math.random()*255
-    return randomRGBvalue
+//deletes old grid so that new grid has space to populate
+function deleteGrid(){
+    // creates object containing all grid columns
+    const allGridCols = document.querySelectorAll('.gridCol');
+    // iterates over grid columns, deleting contents of each, then deleting the column
+    for (let i = allGridCols.length - 1; i >= 0; i--){
+        for (let y = 0; y <= allGridCols.length -1; y++){
+            allGridCols[y].removeChild(allGridCols[y].lastChild);
+        }
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
 }
 
-//allEtchaCol[i].removeChild(allEtchaCol[i].lastChild);
+//converts grid choice from button to number of divs to be generated
+function setGridSize(){    
+    if(userGridSizeInput === "gridSmall"){
+        gridSize = 15;
+    } else if (userGridSizeInput === "gridMedium"){
+        gridSize =  30;
+    } else if (userGridSizeInput === "gridLarge"){
+        gridSize = 70;
+    }
+}
 
+//resets the color based on color button pushed
+function resetColor(){
+    userColorInput = this.id;
+    if (userColorInput === "colorRed"){
+        color = '#C44D58';
+    } else if (userColorInput === "colorBlue"){
+        color = '#4ECDC4';
+    } else if (userColorInput === "colorGreen"){
+        color = '#C7F464';
+    } else if (userColorInput === "colorBlack"){
+        color = 'black';
+    } else if (userColorInput === "colorRandom"){
+        color = 'RANDOM';
+    }
+}
 
-
-
-
-
-
-
+//gets random RGB values and returns in proper RGB format
+function getRandomColor(){
+    let randomRedValue = Math.random()*255
+    let randomGreenValue = Math.random()*255
+    let randomBlueValue = Math.random()*255
+    return "RGB( " + randomRedValue + "," 
+                + randomGreenValue + "," 
+                + randomBlueValue + ")"
+}
 
 
 
