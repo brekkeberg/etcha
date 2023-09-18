856132b0ba07c5f42a911dbc8d27cf25750ee545
brekkeberg <brekke.berg@gmail.com>
2023-09-18 12:07:34 -0700

    delete unecessary file

--- test.js
+++
@@ -1,8 +0,0 @@
-const mainContainer = document.querySelector('#main');
-
-mainContainer.addEventListener('mouseover',testFunc);
-
-function testFunc(){
-    console.log('HELLO WORLD')
-    mainContainer.style.cssText = "background-color: black"
-}
